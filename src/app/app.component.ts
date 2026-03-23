0190e1479a45113e84d0891f0147d33f2258e50c
AndrDemchuk <xdemchuk@gmail.com>
2026-03-23 17:26:27 +0200

    manually invoking change detection (having onPush detectionStrategy set up)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  counter = 0;
+
+  readonly changeDetector = inject(ChangeDetectorRef);
+
+  doNothing() {
+
+  }
+
+  constructor() {
+    setInterval(() => {
+      this.counter++;
+      console.log('Counter', this.counter);
+    }, 3000);
+
+    setInterval(() => {
+      this.changeDetector.detectChanges();
+    }, 5000);
+  }
 
 }
