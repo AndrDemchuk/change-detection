8f964b7fa97ef5a12a9982e81a4f36d29e93ca25
AndrDemchuk <xdemchuk@gmail.com>
2026-03-24 22:23:06 +0200

    rxJs management challange(solved with debounceTime)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
-import { interval } from 'rxjs';
+import { BehaviorSubject, combineLatest, debounceTime, interval, map } from 'rxjs';
+
+type Options = Record<string, string>;
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  readonly counter$ = interval(1000);
+  readonly options$ = new BehaviorSubject<Options>({ 'r': 'Red', 'g': 'Green', 'b': 'Blue' });
+  readonly selectedKey$ = new BehaviorSubject<string>('b');
+  readonly selectedvalue$ = combineLatest([this.options$, this.selectedKey$]).pipe(
+    debounceTime(0),
+    map(([options, key]) => options[key]),
+  );
 
-  /*   readonly changeDetector = inject(ChangeDetectorRef);
-   */
-
-  calculateValue() {
-    console.log('Value is calculated');
-    return 42;
+  switchOptions() {
+    this.options$.next({ 'm': 'Magenta', 'y': 'Yellow', 'c': 'Cyan' });
+    this.selectedKey$.next('c');
   }
-
 
   constructor() {
+    this.selectedvalue$.subscribe(console.log);
   }
-
-
 }
