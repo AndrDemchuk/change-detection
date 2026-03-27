ad2e1103629145cf6ccdf6ae371a7da129e5ffd9
AndrDemchuk <xdemchuk@gmail.com>
2026-03-27 18:50:51 +0200

    turning sum observable into promis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
-import { BehaviorSubject, combineLatest, debounceTime, interval, map } from 'rxjs';
+import { BehaviorSubject, combineLatest, debounceTime, firstValueFrom, interval, map } from 'rxjs';
 
 type Options = Record<string, string>;
 
@@ -12,6 +12,22 @@ type Options = Record<string, string>;
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  readonly a$ = new BehaviorSubject<number>(1);
+  readonly b$ = new BehaviorSubject<number>(2);
+  readonly sum$ = combineLatest([this.a$, this.b$]).pipe(map(([a, b]) => a + b));
+
+  async incA() {
+
+    const sum = await firstValueFrom(this.sum$);
+    if (sum < 10) {
+      this.a$.next(this.a$.value + 1);
+    }
+    // only increment A if A+B is less than 10
+  }
+
+
+
+
   readonly options$ = new BehaviorSubject<Options>({ 'r': 'Red', 'g': 'Green', 'b': 'Blue' });
   readonly selectedKey$ = new BehaviorSubject<string>('b');
   readonly selectedvalue$ = combineLatest([this.options$, this.selectedKey$]).pipe(
